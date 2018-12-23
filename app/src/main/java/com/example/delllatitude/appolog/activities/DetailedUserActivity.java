e33d8e4ff1525e3148670f5acc110e8c358270a0
Damanpreet Singh <damanpreet@Damanpreets-MacBook-Pro.local>
2018-12-23 14:40:03 +0530

    added date field in the detailed user activity's xml

--- app/src/main/java/com/example/delllatitude/appolog/activities/DetailedUserActivity.java
+++ app/src/main/java/com/example/delllatitude/appolog/activities/DetailedUserActivity.java
@@ -10,11 +10,13 @@ import com.example.delllatitude.appolog.models.User;
 
 public class DetailedUserActivity extends AppCompatActivity {
 
+    User user;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_user);
 
-        User user = getIntent().getParcelableExtra("user");
+        user = getIntent().getParcelableExtra("user");
     }
 }
